27666f02e75fbbb1e8ca1b676ceda791a730e0f2
Mateusz Rajek <mateusz_rajek@wp.pl>
2024-03-25 17:41:32 +0100

    Synched with develop

--- App.tsx
+++ App.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
 
-import {SafeAreaView, ScrollView, StatusBar} from 'react-native';
+import { SafeAreaView } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import AppStack from './src/navigation/index';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './src/screens/HomeScreen';
+
+const Stack = createNativeStackNavigator();
 
 function App(): React.JSX.Element {
   return (
     <SafeAreaView>
-      <StatusBar />
-      <ScrollView contentInsetAdjustmentBehavior="automatic"></ScrollView>
+      <NavigationContainer>
+        <Stack.Screen name="Home" component={HomeScreen} />
+
+        {/* <AppStack />
+        <Navigation /> */}
+      </NavigationContainer>
     </SafeAreaView>
   );
 }
